f3a50b5620bd30934928747d3c4f15b5633ff7f9
GGergo <gecsegergo006@gmail.com>
2024-04-26 11:25:04 +0200

    5. feladat

--- fuvar.py
+++ fuvar.py
@@ -17,7 +17,6 @@ adatok = []
 
 for sor in f:
     sor = sor.strip().split(";")
-
     adatok.append(Fuvar(sor[0],sor[1],sor[2],sor[3],float(sor[4].replace(",",".")),float(sor[5].replace(",",".")),sor[6],))
 
 print(f"3. feladat: {len(adatok)-1} fuvar")
@@ -32,4 +31,17 @@ for fuvarok in adatok:
         fuvarszam += 1
 bevetel = str(bevetel)
 print(f"{fuvarszam} fuvar alatt {bevetel.replace(".",",")}$")
+
+print("5. feladat:")
+
+fizetesek = {}
+
+for fuvarok in adatok:
+    if fuvarok.fmod in fizetesek:
+        fizetesek[fuvarok.fmod] += 1
+    else:
+        fizetesek[fuvarok.fmod] = 1
+
+for k,v in fizetesek.items():
+    print(f"\t{k}: {v} fuvar")
         

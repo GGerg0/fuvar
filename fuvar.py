00fc29ce4b67548d615fb94ac73a65948bcac19e
GGergo <gecsegergo006@gmail.com>
2024-04-26 11:19:45 +0200

    4. feladat

--- fuvar.py
+++ fuvar.py
@@ -11,13 +11,25 @@ class Fuvar:
 
 f = open("fuvar.csv", "rt", encoding="utf-8")
 
+f.readline()
+
 adatok = []
 
 for sor in f:
     sor = sor.strip().split(";")
-    adatok.append(Fuvar(sor[0],sor[1],sor[2],sor[3],(sor[4]),float(sor[5]),sor[6],))
+
+    adatok.append(Fuvar(sor[0],sor[1],sor[2],sor[3],float(sor[4].replace(",",".")),float(sor[5].replace(",",".")),sor[6],))
 
 print(f"3. feladat: {len(adatok)-1} fuvar")
 
 
+bevetel = 0
+fuvarszam = 0
+
+for fuvarok in adatok:
+    if fuvarok.id == "6185":
+        bevetel += fuvarok.dij + fuvarok.borravalo
+        fuvarszam += 1
+bevetel = str(bevetel)
+print(f"{fuvarszam} fuvar alatt {bevetel.replace(".",",")}$")
         
